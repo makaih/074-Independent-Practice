fd2cca18490b2c7ca6c4f2eae4ff2e4509284a97
makaih <108191962+makaih@users.noreply.github.com>
2022-06-25 15:37:50 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,20 @@
+
+$(".button-one").click(function() {
+    let review =$("input").val();
+    // alert(review);
+    console.log(review);
+    
+   alert(review);
+    
+   
+    
+    
+    
+    
+  
+    
+    
+    
+});
+
+
